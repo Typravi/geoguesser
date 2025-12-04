a2bed14a05f02bab8aa068720d5e4fc31b291531
lundbergronja <lundbergronja1@gmail.com>
2025-12-04 21:19:35 +0100

    Nya Views, Uppdaterad router

--- src/router/index.js
+++ src/router/index.js
@@ -1,35 +1,60 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import StartView from '../views/StartView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import StartView from "../views/StartView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'Start',
-      component: StartView
-    },
-      {
-      path: '/poll/:id',
-      name: 'PollView',
-      component: () => import('../views/PollView.vue')
+      path: "/",
+      name: "Start",
+      component: StartView,
     },
     {
-      path: '/lobby/:id',
-      name: 'LobbyView',
-      component: () => import('../views/LobbyView.vue')
+      path: "/TestQuestionView",
+      name: "TestQuestionView",
+      component: () => import("../views/NewViews/TestQuestionView.vue"),
     },
     {
-      path: '/create/',
-      name: 'CreateView',
-      component: () => import('../views/CreateView.vue')
+      path: "/QuestionView",
+      name: "QuestionView",
+      component: () => import("../views/NewViews/QuestionView.vue"),
     },
     {
-      path: '/result/:id',
-      name: 'ResultView',
-      component: () => import('../views/ResultView.vue')
-    }
-  ]
-})
+      path: "/JoinGameView",
+      name: "JoinGameView",
+      component: () => import("../views/NewViews/JoinGameView.vue"),
+    },
+    {
+      path: "/NewHomeView",
+      name: "NewHomeView",
+      component: () => import("../views/NewViews/NewHomeView.vue"),
+    },
+    {
+      path: "/ResultView",
+      name: "ResultView",
+      component: () => import("../views/NewViews/ResultView.vue"),
+    },
+    {
+      path: "/poll/:id",
+      name: "PollView",
+      component: () => import("../views/PollView.vue"),
+    },
+    {
+      path: "/lobby/:id",
+      name: "LobbyView",
+      component: () => import("../views/LobbyView.vue"),
+    },
+    {
+      path: "/create/",
+      name: "CreateView",
+      component: () => import("../views/CreateView.vue"),
+    },
+    {
+      path: "/result/:id",
+      name: "ResultView",
+      component: () => import("../views/ResultView.vue"),
+    },
+  ],
+});
 
-export default router
+export default router;
