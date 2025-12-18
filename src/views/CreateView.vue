5ae7ed68f73c99babe3446af3815dac6b335d49b
lundbergronja <lundbergronja1@gmail.com>
2025-12-18 17:56:52 +0100

    nya labels, labels insatta på createview

--- src/views/CreateView.vue
+++ src/views/CreateView.vue
@@ -1,123 +1,118 @@
 <template>
   <div>
+    <p>
+      {{ uiLabels.NumOfQuestions }} {{ numberOfQuestions }}
+      <button @click="decreaseAmount">-</button>
+      <button @click="increaseAmount">+</button>
+    </p>
+    <p>{{ uiLabels.chooseContinent }}</p>
+    <p>
+      <button @click="choseNextContinent"><</button>
+      {{ continent }}
+      <button @click="choseNextContinent">></button>
+    </p>
 
-    <p>Antal frågor: {{ numberOfQuestions }}
-                  <button @click="decreaseAmount">-</button>
-                  <button @click="increaseAmount">+</button>
-  </p>
-  <p>Välj världsdel:</p>
-  <p><button @click="choseNextContinent"> < </button>
-     {{ continent }}
-      <button @click="choseNextContinent"> > </button>
-  </p>
+    <p>
+      <label for="name">{{ uiLabels.yourName }}</label
+      ><br />
+      <input
+        type="text"
+        v-model="playerName"
+        id="playerName"
+        :placeholder="uiLabels.enterYourName"
+      />
+    </p>
 
-
-  <p>
-    <label for="name">Name</label><br>
-    <input type="text"  v-model="playerName" id ="playerName" placeholder="Enter your name">
-  </p>
-
-<p>
-
-<button v-on:click="goToLobby" :disabled="!playerName.trim()">
-  {{ uiLabels.createGame}}
-  </button>
-
-</p>
+    <p>
+      <button v-on:click="goToLobby" :disabled="!playerName.trim()">
+        {{ uiLabels.createGame }}
+      </button>
+    </p>
   </div>
-
-
 </template>
 
 <script>
-import io from 'socket.io-client';
+import io from "socket.io-client";
 import { getRandomCity } from "@/assets/logic.js";
 import continentData from "@/assets/maps.json";
 const socket = io("localhost:3000");
 
 export default {
-  name: 'CreateView',
+  name: "CreateView",
   data: function () {
     return {
       lang: localStorage.getItem("lang") || "en",
-      playerName:"",
+      playerName: "",
       uiLabels: {},
       numberOfQuestions: 1,
-      lobbyID:null,
-      continent:'europe',
+      lobbyID: null,
+      continent: "europe",
       cities: [],
       round: null,
-    }
+    };
   },
   created: function () {
-    socket.on( "uiLabels", labels => this.uiLabels = labels );
-    socket.on("gameData", data => {
+    socket.on("uiLabels", (labels) => (this.uiLabels = labels));
+    socket.on("gameData", (data) => {
       console.log("Lobby created:", data);
-      this.$router.push(`/lobby/${this.lobbyID}/${data.hostName}`); 
+      this.$router.push(`/lobby/${this.lobbyID}/${data.hostName}`);
       console.log("Lista med alla städer", this.cities);
     });
-    
-    socket.emit( "getUILabels", this.lang );
 
-   
+    socket.emit("getUILabels", this.lang);
   },
   methods: {
-
     getGameID() {
-    const lobbyID = Math.floor(100000 + Math.random() * 900000);
-    return lobbyID;
+      const lobbyID = Math.floor(100000 + Math.random() * 900000);
+      return lobbyID;
     },
 
     goToLobby() {
- 
-  this.lobbyID= this.getGameID();
-  this.getCitiesForContinentInArray();
-  socket.emit("createGame", {
-    lobbyID: this.lobbyID,
-    lang: this.lang,
-    playerName: this.playerName,
-    numberOfQuestions: this.numberOfQuestions,
-    continent: this.continent,
-    cities: this.cities,
-    round: 1
-  });
+      this.lobbyID = this.getGameID();
+      this.getCitiesForContinentInArray();
+      socket.emit("createGame", {
+        lobbyID: this.lobbyID,
+        lang: this.lang,
+        playerName: this.playerName,
+        numberOfQuestions: this.numberOfQuestions,
+        continent: this.continent,
+        cities: this.cities,
+        round: 1,
+      });
+    },
 
+    getCitiesForContinentInArray() {
+      this.cities = [];
 
-  },
+      const continentKey = this.continent.toLowerCase();
+      const continentObj = continentData[continentKey];
 
-  getCitiesForContinentInArray() {
-  this.cities = []
+      if (!continentObj) {
+        console.error("Okänd kontinent:", this.continent);
+        return;
+      }
 
-  const continentKey = this.continent.toLowerCase()
-  const continentObj = continentData[continentKey]
-
-  if (!continentObj) {
-    console.error("Okänd kontinent:", this.continent)
-    return
-  }
-
-  for (let i = 0; i < this.numberOfQuestions; i++) {
-    const city = getRandomCity(continentObj)
-    this.cities.push(city)
-  }
-},
+      for (let i = 0; i < this.numberOfQuestions; i++) {
+        const city = getRandomCity(continentObj);
+        this.cities.push(city);
+      }
+    },
     increaseAmount() {
-    if (this.numberOfQuestions < 10) {
-    this.numberOfQuestions++;}
+      if (this.numberOfQuestions < 10) {
+        this.numberOfQuestions++;
+      }
     },
     decreaseAmount() {
-    if (this.numberOfQuestions > 1) {
-      this.numberOfQuestions--;}
-  },
+      if (this.numberOfQuestions > 1) {
+        this.numberOfQuestions--;
+      }
+    },
     choseNextContinent() {
-      const continents = ['africa', 'europe', 'Planet earth'];
+      const continents = ["africa", "europe", "Planet earth"];
       const currentIndex = continents.indexOf(this.continent);
       const nextIndex = (currentIndex + 1) % continents.length;
-      this.continent = continents[nextIndex];}
-
+      this.continent = continents[nextIndex];
+    },
   },
-
-    
-    }
-
+};
 </script>
