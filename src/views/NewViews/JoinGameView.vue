55208426de73ba86234c5b4489b8161b2e2fe54b
lundbergronja <lundbergronja1@gmail.com>
2025-12-18 21:39:40 +0100

    Nya labels för joinGameView

--- src/views/NewViews/JoinGameView.vue
+++ src/views/NewViews/JoinGameView.vue
@@ -1,92 +1,100 @@
 <template>
-
   <div>
     <p>
-      <label for="name">Name</label><br>
-      <input type="text"  v-model="playerName" id ="playerName" placeholder="Enter your name">
-  </p>
+      <label for="name">{{ uiLabels.yourName }}</label
+      ><br />
+      <input
+        type="text"
+        v-model="playerName"
+        id="playerName"
+        :placeholder="uiLabels.enterYourName"
+      />
+    </p>
 
-  <p>
-      <label for="lobbyID">Lobby</label><br>
-      <input type="text" @input="checkgameID" id="lobbyID" v-model="lobbyID" placeholder="Lobby ID"> 
-         
-  </p>
-  <p v-if="gameIsChecked && !gameExists" class="error-message">
+    <p>
+      <label for="lobbyID"> {{ uiLabels.gameID }} </label><br />
+      <input
+        type="text"
+        @input="checkgameID"
+        id="lobbyID"
+        v-model="lobbyID"
+        :placeholder="uiLabels.enterGameID"
+      />
+    </p>
+    <p v-if="gameIsChecked && !gameExists" class="error-message">
       <!--  lägg till felmedellande här-->
-  </p>
-      <p>
-  <button v-on:click="joinGame" :disabled="!gameExists || !gameIsChecked || !playerName.trim()">
-    {{ uiLabels.joinGame}}
-    </button>
-  </p>
-    </div>
-  
-  
-  </template>
-  
-  <script>
-  import io from 'socket.io-client';
-  const socket = io("localhost:3000");
-  
-  export default {
-    name: 'JoinGameView',
-    data: function () {
-      return {
-        lang: localStorage.getItem("lang") || "en",
-        playerName:"",
-        lobbyID: "",  
-        uiLabels: {},
-        gameExists: false,
-        gameIsChecked: false,
-      }
-    },
-    created: function () {
-      socket.on( "uiLabels", labels => this.uiLabels = labels );
-      socket.emit( "getUILabels", this.lang );
+    </p>
+    <p>
+      <button
+        v-on:click="joinGame"
+        :disabled="!gameExists || !gameIsChecked || !playerName.trim()"
+      >
+        {{ uiLabels.joinGame }}
+      </button>
+    </p>
+  </div>
+</template>
 
-      
-  socket.on("playerJoined", () => {
-    
-    this.$router.push(`/lobby/${this.lobbyID}/${this.playerName}`);
-  });
+<script>
+import io from "socket.io-client";
+const socket = io("localhost:3000");
 
-      socket.on("lobbyError", msg => {
-        console.log("Lobby error:", msg);
-      alert(msg); 
-});
-    },
-    methods: {
+export default {
+  name: "JoinGameView",
+  data: function () {
+    return {
+      lang: localStorage.getItem("lang") || "en",
+      playerName: "",
+      lobbyID: "",
+      uiLabels: {},
+      gameExists: false,
+      gameIsChecked: false,
+    };
+  },
+  created: function () {
+    socket.on("uiLabels", (labels) => (this.uiLabels = labels));
+    socket.emit("getUILabels", this.lang);
 
-      checkgameID() {
+    socket.on("playerJoined", () => {
+      this.$router.push(`/lobby/${this.lobbyID}/${this.playerName}`);
+    });
 
-      this.lobbyID = this.lobbyID.replace(/\D/g, '');
+    socket.on("lobbyError", (msg) => {
+      console.log("Lobby error:", msg);
+      alert(msg);
+    });
+  },
+  methods: {
+    checkgameID() {
+      this.lobbyID = this.lobbyID.replace(/\D/g, "");
 
-      console.log('[JoinGameView] checkgameID, lobbyID=', this.lobbyID);
+      console.log("[JoinGameView] checkgameID, lobbyID=", this.lobbyID);
 
-      if(this.lobbyID.length < 6) {
-        this.gameIsChecked = false
+      if (this.lobbyID.length < 6) {
+        this.gameIsChecked = false;
       }
       if (this.lobbyID.length === 6) {
-        console.log('[JoinGameView] emitting validateLobbyID for', this.lobbyID);
-        socket.emit('validateLobbyID', this.lobbyID, (exists) => {
-        console.log('[JoinGameView] validateLobbyID callback, exists=',exists);
-        this.gameExists = exists;
-        this.gameIsChecked = true;
-          });
-        };
+        console.log(
+          "[JoinGameView] emitting validateLobbyID for",
+          this.lobbyID
+        );
+        socket.emit("validateLobbyID", this.lobbyID, (exists) => {
+          console.log(
+            "[JoinGameView] validateLobbyID callback, exists=",
+            exists
+          );
+          this.gameExists = exists;
+          this.gameIsChecked = true;
+        });
+      }
     },
-  
-    joinGame() {
 
+    joinGame() {
       socket.emit("participateInGame", {
         lobbyID: this.lobbyID,
-        playerName: this.playerName
+        playerName: this.playerName,
       });
-
-      
-
-      }
-  
-  }
-}
-  </script>
+    },
+  },
+};
+</script>
