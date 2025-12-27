a52e43b2fd0e4c44f871346511f40cfe7aaaef34
lundbergronja <lundbergronja1@gmail.com>
2025-12-27 12:09:56 +0100

    calculatePunishment

--- src/assets/logic.js
+++ src/assets/logic.js
@@ -24,3 +24,17 @@ export function getRandomContinent(allContinents) {
     continentNames[Math.floor(Math.random() * continentNames.length)];
   return { name: randomContinent, map: allContinents[randomContinent] };
 }
+
+//Straffet vid uteblivet klick avrundas uppåt till närmsta steg 1000,1500,2000 osv
+// Notera att maxDistance måste räknas ut i GeoMapView som de ser ut nu
+export function calculatePunishment (maxDistance) {
+  const minPunishment = 1000;
+  const extraProcentPunishment = 1.1;
+  const step = 500;
+
+  const extraTimesMax = extraProcentPunishment*maxDistance
+
+  const punishmentRoundedUpToNextStep =
+  Math.ceil(extraTimesMax/step)*step;
+  return Math.max(minPunishment, punishmentRoundedUpToNextStep);
+}
